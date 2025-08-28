951b9d6a622b295cd4eb5645e9fab1719a15c353
687edf53 <sm3257698@gmail.com>
2025-08-28 14:05:59 +0200

    add some improvments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Nav from './components/Nav'
-import './App.css'
 
 function App() {
   return (
     <>
     <Nav />
-      hello
     </>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
 export default function Nav() {
   return (
     <>
-      <nav className="navbar fixed top-0 w-full left-0 flex content-around">
-        <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
-        <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
+      <nav className="w-full flex justify-between">
+        <div className="navbar">
+          <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
+          <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
+        <div/>
       </nav>
     </>
   );
