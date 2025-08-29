46655f41ac9d30dba72c508160e2c52112c97fce
687edf53 <sm3257698@gmail.com>
2025-08-29 15:48:43 +0300

    very few navbar edits

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,22 @@
+
 export default function Nav() {
   return (
     <>
-      <nav className="w-full flex justify-between">
-        <div className="navbar">
-          <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
-          <div className="hover:bg-gray-500 duration-150 text-gray-700 inline-block p-2 rounded-xl bg-gray-400">logo</div>
-        <div/>
+      <nav className="w-full ">
+        <div className="navbar flex justify-between px-16 bg-red-500">
+          <div className="font-bold text-2xl ">
+            Logo
+          </div>
+          <div>
+            <ul className="flex ">
+              <li className="px-2 cursor-pointer text-cyan-500 font-medium bg-amber-500 rounded-xl mx-2">Home</li>
+              <li className="px-2 cursor-pointer text-cyan-500 font-medium bg-amber-500 rounded-xl mx-2">Dashboard</li>
+              <li className="px-2 cursor-pointer text-cyan-500 font-medium bg-amber-500 rounded-xl mx-2">Services</li>
+              <li className="px-2 cursor-pointer text-cyan-500 font-medium bg-amber-500 rounded-xl mx-2">Contact</li>
+              <li className="px-2 cursor-pointer text-cyan-500 font-medium bg-amber-500 rounded-xl mx-2">About</li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </>
   );
